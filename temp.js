3bc317b9e6b47db348fdeafe459a5a175e263893
Hrishit <hrishit.bp@gmail.com>
2022-05-01 17:02:50 +0530

    Making changes

---
+++ temp.js
@@ -0,0 +1,10 @@
+
+var okbutton=document.getElementById('enter');
+okbutton.addEventListener('click',passw);
+function passw(){
+    console.log("Hello");
+    var str=document.getElementById('password').value;
+    
+    console.log(str2);
+}
+
