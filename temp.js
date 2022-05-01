112ac6ccad02fbadd9d3c6e7b90df0391b7d1184
Hrishit <hrishit.bp@gmail.com>
2022-05-01 18:47:08 +0530

    temporary check

--- temp.js
+++ temp.js
@@ -1,37 +1,22 @@
-// const req = require('express');
-
-// const app= express()
-
-// app.use(express.json())
-
-// const users= []
-
-// app.get('/users', (req, res)=>{
-//   res.json(users)
-// })
-
-// app.post('/users', (req, res)=>{
-//   const user = {name: req.body.name, password: req.body.password}
-//   users.push(user)
-//   res.status(201).send()
-// })
-
-// app.listen(3000)
+const pass=['a','b','c','d','e','f','g','h','i','j','k','l','m','n']
 var okbutton=document.getElementById('enter');
 
 
 okbutton.addEventListener('click',passw);
 function passw(){
+    var flag=false;
     console.log("Hello");
     var str=document.getElementById('password').value;
     const data={str};
-    // const options={
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data),
-    // };
-    // fetch('/api',options);
+    for(var i=0;i<14;i++) {
+        if(str== pass[i]){
+            flag=true;
+            break;
+        }
+    }
+    if(flag){
+        window.location.href="./main.html";
+
+    }
 }
 
--- temp2.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const app= express();
-// app.listen(3000,() => console.log('listening at 3000'));
-// app.use(express.express.static('public'));
-// app.post('/api',(request,response)=>{
-//     console.log(request.body);
-// });
