5aca65e99aeb9e49146c5db88bc8661706470b9e
Hrishit <hrishit.bp@gmail.com>
2022-05-01 18:30:28 +0530

    temp

--- temp.js
+++ temp.js
@@ -1,10 +1,37 @@
+// const req = require('express');
 
+// const app= express()
+
+// app.use(express.json())
+
+// const users= []
+
+// app.get('/users', (req, res)=>{
+//   res.json(users)
+// })
+
+// app.post('/users', (req, res)=>{
+//   const user = {name: req.body.name, password: req.body.password}
+//   users.push(user)
+//   res.status(201).send()
+// })
+
+// app.listen(3000)
 var okbutton=document.getElementById('enter');
+
+
 okbutton.addEventListener('click',passw);
 function passw(){
     console.log("Hello");
     var str=document.getElementById('password').value;
-    
-    console.log(str2);
+    const data={str};
+    // const options={
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data),
+    // };
+    // fetch('/api',options);
 }
 
